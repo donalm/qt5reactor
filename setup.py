bb659760833b156bdff630404fa31aad5ebde5b2
Dónal McMullan <donal.mcmullan@gmail.com>
2020-10-08 21:55:46 +0100

    Drop python requirement to 2.7

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ classifiers = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
@@ -38,7 +39,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     keywords=['Qt', 'twisted'],
-    python_requires='>=3.5',
+    python_requires='>=2.7',
     install_requires=['twisted'],
     extras_require={
         "pyqt5": [
